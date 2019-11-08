6cd864875b4850dcd229d589994bcad57999b99b
Adam Rozenek <adam.rozenek@gmail.com>
2019-11-08 22:24:13 +0100

    Checking transition

--- petrinet/src/petrinet/PetriNet.java
+++ petrinet/src/petrinet/PetriNet.java
@@ -1,23 +1,36 @@
 package petrinet;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 public class PetriNet<T> {
 
-    public PetriNet(Map<T, Integer> initial, boolean fair) {
+    private Map<T, Integer> places;
+    private boolean fair;
 
+    public PetriNet(Map<T, Integer> initial, boolean fair) {
+        places = new HashMap<>(initial);
+        this.fair = fair;
     }
 
     public Set<Map<T, Integer>> reachable(Collection<Transition<T>> transitions) {
-
+        return null;
     }
 
     public Transition<T> fire(Collection<Transition<T>> transitions) throws InterruptedException {
 
+        return null;
     }
 
+    private boolean enabledTransition(Transition<T> transition) {
+        Iterator<Map.Entry<T, Integer>> iter = transition.arcIterator();
+        for (Map.Entry<T, Integer> arc: iter) {
+
+            if (places.get(arc.getKey()) != arc.getValue()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
--- petrinet/src/petrinet/Transition.java
+++ petrinet/src/petrinet/Transition.java
@@ -1,14 +1,50 @@
 package petrinet;
 
+import java.util.AbstractMap;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Map;
 
 
 public class Transition<T> {
 
-    public Transition(Map<T, Integer> input, Collection<T> reset, Collection<T> inhibitor, Map<T, Integer> output) {
+    private Map<T, Integer> input;
+    private Collection<T> reset;
+    private Collection<T> inhibitor;
+    private Map<T, Integer> output;
 
+    public Transition(Map<T, Integer> input, Collection<T> reset, Collection<T> inhibitor, Map<T, Integer> output) {
+        this.input = new HashMap<>(input);
+        this.reset = new LinkedList<>(reset);
+        this.inhibitor = new LinkedList<>(inhibitor);
+        this.output = new HashMap<>(output);
     }
 
+    public ArcIterator arcIterator() {
+        return new ArcIterator();
+    }
 
+    class ArcIterator implements Iterator<Map.Entry<T, Integer>> {
+        Iterator<Map.Entry<T, Integer>> inputIter;
+        Iterator<T> inhibitorIter;
+        public ArcIterator() {
+            inputIter = input.entrySet().iterator();
+            inhibitorIter = inhibitor.iterator();
+        }
+        @Override
+        public boolean hasNext() {
+            return inputIter.hasNext() || inhibitorIter.hasNext();
+        }
+        @Override
+        public Map.Entry<T, Integer> next() {
+            if (inputIter.hasNext()) {
+                return inputIter.next();
+            } else if (inhibitorIter.hasNext()) {
+                return new AbstractMap.SimpleEntry<>(inhibitorIter.next(), 0);
+            }
+            return null;
+        }
+    }
 }
