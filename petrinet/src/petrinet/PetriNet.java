b17c6f88966c90789f02a4f90bef9489931fe06a
Adam Rozenek <adam.rozenek@gmail.com>
2019-11-11 10:08:51 +0100

    Checking transition

--- petrinet/src/petrinet/PetriNet.java
+++ petrinet/src/petrinet/PetriNet.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class PetriNet<T> {
 
     private Map<T, Integer> places;
-    private boolean fair;
+    private final boolean fair;
 
     public PetriNet(Map<T, Integer> initial, boolean fair) {
         places = new HashMap<>(initial);
@@ -22,13 +22,22 @@ public class PetriNet<T> {
         return null;
     }
 
-    private boolean enabledTransition(Transition<T> transition) {
-        Iterator<Map.Entry<T, Integer>> iter = transition.arcIterator();
-        for (Map.Entry<T, Integer> arc: iter) {
-
-            if (places.get(arc.getKey()) != arc.getValue()) {
+    private boolean testEnabledTransition(Transition<T> transition) {
+        if (transition == null || !transition.arcConflict()) {
+            return false;
+        }
+        Iterator<T> inhibitorIterator = transition.inhibitorIterator();
+        while (inhibitorIterator.hasNext()) {
+            T place = inhibitorIterator.next();
+            if (places.containsKey(place) && places.get(place) > 0)
                 return false;
-            }
+        }
+        Iterator<Map.Entry<T, Integer>> inputIterator = transition.inputIterator();
+        while (inputIterator.hasNext()) {
+            Map.Entry<T, Integer> arc = inputIterator.next();
+            if (!places.containsKey(arc.getKey()) ||
+                    places.get(arc.getKey()) < arc.getValue())
+                return false;
         }
         return true;
     }
--- petrinet/src/petrinet/Transition.java
+++ petrinet/src/petrinet/Transition.java
@@ -14,37 +14,42 @@ public class Transition<T> {
     private Collection<T> reset;
     private Collection<T> inhibitor;
     private Map<T, Integer> output;
+    private final boolean alwaysDisenable;
 
     public Transition(Map<T, Integer> input, Collection<T> reset, Collection<T> inhibitor, Map<T, Integer> output) {
         this.input = new HashMap<>(input);
         this.reset = new LinkedList<>(reset);
         this.inhibitor = new LinkedList<>(inhibitor);
         this.output = new HashMap<>(output);
-    }
-
-    public ArcIterator arcIterator() {
-        return new ArcIterator();
-    }
-
-    class ArcIterator implements Iterator<Map.Entry<T, Integer>> {
-        Iterator<Map.Entry<T, Integer>> inputIter;
-        Iterator<T> inhibitorIter;
-        public ArcIterator() {
-            inputIter = input.entrySet().iterator();
-            inhibitorIter = inhibitor.iterator();
-        }
-        @Override
-        public boolean hasNext() {
-            return inputIter.hasNext() || inhibitorIter.hasNext();
-        }
-        @Override
-        public Map.Entry<T, Integer> next() {
-            if (inputIter.hasNext()) {
-                return inputIter.next();
-            } else if (inhibitorIter.hasNext()) {
-                return new AbstractMap.SimpleEntry<>(inhibitorIter.next(), 0);
+        boolean alwaysDisenable = true;
+        for (T t: reset) {
+            if (input.containsKey(t) && input.get(t) > 0) {
+                alwaysDisenable = false;
+                break;
             }
-            return null;
         }
+        this.alwaysDisenable = alwaysDisenable;
     }
+
+
+    public Iterator<T> resetIterator() {
+        return reset.iterator();
+    }
+
+    public Iterator<T> inhibitorIterator() {
+        return inhibitor.iterator();
+    }
+
+    public Iterator<Map.Entry<T, Integer>> inputIterator() {
+        return input.entrySet().iterator();
+    }
+
+    public Iterator<Map.Entry<T, Integer>> outputIterator() {
+        return output.entrySet().iterator();
+    }
+
+    public boolean arcConflict() {
+        return alwaysDisenable;
+    }
+
 }
